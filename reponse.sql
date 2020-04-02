1226be7d97d844fff27c26e7a6456c2d5f53755d
AudreyBclz <audrey.bucholz@hotmail.fr>
2020-04-02 17:26:57 +0200

    modif avec les noms et champs de la DB

--- reponse.sql
+++ reponse.sql
@@ -1,67 +1,68 @@
 -- 1 --
-SELECT *
-FROM products AS prod, categories AS cat, brand AS br
+SELECT name, model_year, price,nameCat,brand_name
+FROM products AS prod, categories AS cat, brands AS br
 WHERE br.brand_name="Haro"
-AND cat.category_name="Electric Bikes"
-AND br.brand_id=prod.brand_id
-AND cat.category_id=prod.category_id;
+AND cat.nameCat="Electric Bikes"
 
 
 -- 2 --
-SELECT *
-FROM products AS prod, categories AS cat, brand AS br
+SELECT
+	prod.name,
+    prod.model_year,
+    prod.price,
+    cat.nameCat,
+    br.brand_name,
+    ROUND(prod.price*0.2,2) AS MontantTVA,
+    ROUND(prod.price*1.2,2) AS PrixTTC
+FROM products AS prod, categories AS cat, brands AS br
 WHERE br.brand_name="Haro"
-AND cat.category_name="Electric Bikes"
-AND br.brand_id=prod.brand_id
-AND cat.category_id=prod.category_id
-AND prod.list_price * 0.2 AS MONTANT_TVA,
-AND SUM(prod.list_price,MONTANT_TVA) AS pTTC;
+AND cat.nameCat="Electric Bikes"
 
 
 -- 3 --
-SELECT prod.product_name, prod.list_price
+SELECT prod.name
 FROM products AS prod
-WHERE prod.list_price >=500
-AND prod.list_price<=1500;
+WHERE prod.price >=500
+AND prod.price<=1500
 
 
 -- 4 --
-SELECT prod.product_name, prod.model_year, prod.list_price, br.brand_name
-FROM products AS prod, brand AS br
-WHERE br.brand_name LIKE 'H%';
+SELECT prod.name, prod.model_year, prod.price
+FROM products AS prod, brands AS br
+WHERE br.brand_name LIKE 'H%'
 
 
 -- 5 --
-SELECT prod.product_name, prod.model_year, prod.list_price
-FROM products AS prod, brand AS br
-WHERE prod.product_name LIKE '%Ice%';
+SELECT prod.name, prod.model_year, prod.price
+FROM products AS prod
+WHERE prod.name LIKE '%lce%' -- Je ne savais pas si c'était un l ou un i, je trouve une occurence avec lce --
 
 
 -- 6 --
 DELETE
-FROM products as prod
-WHERE prod.brand_id=9;
+FROM products
+WHERE brand_id=9;
 
 
 -- 7 --
 DELETE
-FROM products as prod
-WHERE prod.category_id=6;
+FROM products
+WHERE category_id=6;
 
 
 -- 8 --
 UPDATE products
-SET list_price=1499 WHERE product_id=9;
+SET price=1499 WHERE id=9; -- Il n'y a pas de produit avec id 9 dans la DB --
 
 
 -- 9 --
-INSERT INTO category (category_name)
-VALUES ('Roller skates');
+INSERT INTO categories (idCat,nameCat)
+VALUES (8,'Roller skates')
 
 
 -- 10 --
-INSERT INTO products(products_name,brand_id,category_id,model_year,list_price)
-VALUES('roller skates cool',2,8,2020,258);
+INSERT INTO products(id,name,brand_id,category_id,model_year,price)
+VALUES(15,'roller skates cool',2,8,2020,258);
 
 
 
