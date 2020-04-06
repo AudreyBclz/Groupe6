b1b82ac420c169856d010da26e7fc891ca0814c7
AudreyBclz <audrey.bucholz@hotmail.fr>
2020-04-06 13:10:35 +0200

    Ajout exemple Site

---
+++ exempleSite/mesVehicules.php
@@ -0,0 +1,49 @@
+<?php
+$tabVehicule =array("Renault"=>"R25",
+                    "Ford"=>"Focus",
+                    "Peugeot"=>"206",
+                    "Dacia"=>"Duster",
+                    "Delorean"=>"DMC-12",
+                    "Citroen"=>"2CV",
+                    "Pontiac"=>"Trans Am noire",
+                    "Dodge"=>"Charger"
+    );
+?>
+<!DOCTYPE>
+<html>
+    <head>
+        <meta charset="UTF-8"/>
+        <style>
+            td
+            {
+                border: 1px solid black;
+            }
+            table
+            {
+                border-collapse: collapse;
+            }
+        </style>
+    </head>
+    <body>
+        <h1> Voici la liste de mes véhicules</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>Marque</th>
+                    <th>Modèle</th>
+                </tr>
+            </thead>
+            <tbody>
+            <?php
+            foreach ($tabVehicule as $marque=> $modele)
+            {
+                echo '<tr>
+                        <td>'.$marque.'</td>
+                        <td>'.$modele.'</td>
+                       <tr/>';
+            }
+            ?>
+            </tbody>
+        </table>
+    </body>
+</html>
