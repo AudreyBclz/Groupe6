54e2a47a304004a6dc585a7d6ab7e5e1297b747d
AudreyBclz <audrey.bucholz@hotmail.fr>
2020-05-07 16:47:23 +0200

    affichage via base de donnée et ajout des articles dans le panier

--- ProjetStage/src/views/voirplus.php
+++ ProjetStage/src/views/voirplus.php
@@ -1,53 +1,135 @@
 <?php
 require_once 'elements/head.php';
 require_once 'elements/footer.php';
-
+require_once '../config/config.php';
+require_once '../models/connect.php';
+session_start();
 head();
+$db=connect();
+
+$sqlSelCaf='SELECT * FROM cafe
+            INNER JOIN pays ON pays_idpays=idpays
+            WHERE idcafe=:id';
+$reqSelCaf=$db->prepare($sqlSelCaf);
+$reqSelCaf->bindParam(':id',$_GET['id']);
+$reqSelCaf->execute();
+$tab_cafe=array();
+while($data=$reqSelCaf->fetchObject())
+{
+    array_push($tab_cafe,$data);
+}
+if($tab_cafe[0]->typeCafe==='En grain')
+{
+    $suffixe=' le sachet d\'1 kg';
+}
+else
+{
+    $suffixe=' le paquet de 250g';
+}
+if(isset($_GET['id']) && isset($_GET['quantite']))
+{
+    if(isset($_SESSION['nom']) && isset($_SESSION['prenom']) && isset($_SESSION['iduser']) && isset($_SESSION['role'])) {
+        $sqlSelPanier = 'SELECT * FROM panier
+                        WHERE  users_idUsers=:id
+                        AND cafe_idcafe=:id_c';
+        $reqSelPanier = $db->prepare($sqlSelPanier);
+        $reqSelPanier->bindParam('id', $_SESSION['iduser']);
+        $reqSelPanier->bindParam(':id_c', $_GET['id']);
+        $reqSelPanier->execute();
+        $tab_panier = array();
+        while ($data = $reqSelPanier->fetchObject()) {
+            array_push($tab_panier, $data);
+        }
+        if (!empty($tab_panier)) {
+            $qte = intval($tab_panier[0]->quantite);
+            $sqlUpPan = 'UPDATE panier
+                        SET quantite=:quant
+                        WHERE users_idUsers=:id
+                        AND cafe_idcafe=:id_c';
+            $reqUpPan = $db->prepare($sqlUpPan);
+            $quantite = $qte + $_GET['quantite'];
+            $reqUpPan->bindParam(':quant', $quantite);
+            $reqUpPan->bindParam(':id', $_SESSION['iduser']);
+            $reqUpPan->bindParam(':id_c', $_GET['id']);
+            $reqUpPan->execute();
+            echo '<div class="alert-info p-2 text-center">Articles ajouté à votre panier</div>';
+        } else {
+            $sqlSelAd = 'SELECT adresse_idadresse FROM users
+                        WHERE idUsers=:id';
+            $reqSelAd = $db->prepare($sqlSelAd);
+            $reqSelAd->bindParam(':id', $_SESSION['iduser']);
+            $reqSelAd->execute();
+            $tab_Ad = array();
+            while ($data = $reqSelAd->fetchObject()) {
+                array_push($tab_Ad, $data);
+            }
+            $idAd = intval($tab_Ad[0]->adresse_idadresse);
+           
+            $sqlInsPan = 'INSERT INTO panier (users_idUsers,cafe_idcafe,quantite,adresse_idadresse)
+                        VALUES (:id_u,:id_c,:qte,:ad)';
+            $reqInsPan = $db->prepare($sqlInsPan);
+            $reqInsPan->bindParam('id_u', $_SESSION['iduser']);
+            $reqInsPan->bindParam('id_c', $_GET['id']);
+            $reqInsPan->bindParam(':qte', $_GET['quantite']);
+            $reqInsPan->bindParam(':ad', $idAd);
+            $reqInsPan->execute();
+            echo '<div class="alert-info p-2 text-center">Articles ajouté à votre panier</div>';
+
+        }
+    }else
+    {
+        echo'<div class="alert-warning p-2 text-center">Vous devez vous connecter pour ajouter dans votre panier <a href="connexion.php" class="btn btn-marron">Connexion</a></div>';
+    }
+}
+
+
 ?>
-    <div class="container">
-        <div class="arr_plan row justify-content-center">
-            <h1 class="text-center titre">Nom du café</h1>
+
+        <div class="container">
+            <div class="arr_plan row justify-content-center">
+                <h1 class="text-center titre"><?= $tab_cafe[0]->nomCafe ?></h1>
+
                 <div class="row justify-content-between">
                     <div class="col-xl-5 col-lg-5 col-md-12 col-sm-12 mb-3">
-                        <img src="../../public/img/cafe-vrac.png" class="panneau w-100 bg-light rounded" alt="...">
+                        <img src="../../public/img/<?= $tab_cafe[0]->photoCafe ?>" class="panneau w-100 bg-light rounded"
+                             alt="...">
                         <div class="d-flex justify-content-between mt-3">
-                            <p><span>Pays:</span><span class="p-2 m-1 rounded bg-wheat font-weight-bold">Honduras</span></p>
-                            <p><span>Type :</span><span class="p-2 m-1 rounded bg-light font-weight-bold">En grain</span></p>
+                            <p><span>Pays:</span><span
+                                        class="p-2 m-1 rounded bg-wheat font-weight-bold"><?= $tab_cafe[0]->nomPays ?></span>
+                            </p>
+                            <p><span>Type :</span><span
+                                        class="p-2 m-1 rounded bg-light font-weight-bold"><?= $tab_cafe[0]->typeCafe ?></span>
+                            </p>
                         </div>
                     </div>
                     <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 mb-3">
                         <div class="d-flex justify-content-between mb-3 ">
-                            <img src="../../public/img/deca.png" class="w-25">
-                            <img src="../../public/img/bio.png" class="w-50">
+                            <img src="../../public/img/deca.png" class="w-25 <?php if ($tab_cafe[0]->decafCafe !== "1") {
+                                echo 'invisible';
+                            } ?>">
+                            <img src="../../public/img/bio.png" class="w-50 <?php if ($tab_cafe[0]->bioCafe !== "1") {
+                                echo 'invisible';
+                            } ?>">
                         </div>
                         <div class="bg-marron2 p-3 rounded">
-                                <h5 class="card-title">Un café fort en goût</h5>
-                                <p class="card-text">
-
-                                    Aenean pharetra orci id vehicula efficitur. Nam euismod, neque quis pharetra luctus, felis sapien
-                                    vestibulum erat, vel porta orci turpis et mi. Ut vel mauris vitae orci fermentum elementum.
-                                    In efficitur, velit ac luctus eleifend, arcu massa lobortis erat, sed luctus libero odio eget orci.
-                                    Nunc non velit mi. Suspendisse quis pulvinar lectus. Vestibulum blandit risus id risus consequat, a efficitur ex tempor.
-                                    Vestibulum aliquet ullamcorper velit a efficitur. </p>
+                            <p class="card-text"><?= $tab_cafe[0]->descCafe ?></p>
                         </div>
                         <div class="card-footer rounded mb-3">
-                            <p>Prix:<span class="font-weight-bold p-2">25€</span> le sachet d'1kg<p>
+                            <p>Prix:<span class="font-weight-bold p-2"><?= $tab_cafe[0]->prixCafe ?>€</span><?= $suffixe ?>
+                            <p>
                         </div>
-                        <form method="post" action="panier.php" class="d-flex justify-content-between">
+                        <form method="get" action="voirplus.php" class="d-flex justify-content-between">
                             <div class="d-flex">
                                 <label for="quantite" class="mr-2 col-form-label">Qté:</label>
                                 <input type="number" name="quantite" id="quantite" class=" form-control w-25">
+                                <input type="number" name="id" value="<?= $tab_cafe[0]->idcafe ?>" class="d-none"/>
                             </div>
                             <button type="submit" class="btn btn-marron">Ajouter au panier</button>
                         </form>
-
-                            </div>
-                        </div>
-                </div>
                     </div>
-
                 </div>
+            </div>
         </div>
-    </div>
-<?php
+
+        <?php
 footer();
