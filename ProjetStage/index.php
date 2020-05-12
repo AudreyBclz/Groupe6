fa5bf3653b58a82f07699fb7827588aa5eed6d74
AudreyBclz <audrey.bucholz@hotmail.fr>
2020-05-12 16:34:57 +0200

    modif d'un style id en style class

--- ProjetStage/index.php
+++ ProjetStage/index.php
@@ -19,7 +19,7 @@ require_once 'src/views/elements/footer.php';
     <body class="bg-vert">
         <div class=" container d-flex flex-column">
             <h1 class="text-center mt-2 mb-5 titre">Bienvenue chez Paradise Coffee!!</h1>
-            <h2 class="text-center mt-5" id="s_titre"> Entrer et laisser vous tenter!</h2>
+            <h2 class="text-center s_titre mt-5"> Entrer et laisser vous tenter!</h2>
             <div class="text-center mt-5">
                 <a href="src/views/selection.php"><img src="public/img/tasseCafe.png" id="logo"/></a>
             </div>
