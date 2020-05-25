f2ba57bf09f331f8badbfd352307a4d6ef0748e2
AudreyBclz <61549680+AudreyBclz@users.noreply.github.com>
2020-05-25 16:55:48 +0200

    suppression du head inutile

--- ProjetStage/src/views/home.php
+++ ProjetStage/src/views/home.php
@@ -1,19 +1,6 @@
 <?php
  ?>
-    <!doctype html>
-    <html lang="fr">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, user-scalable=no">
 
-
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-        <link rel="stylesheet" href="public/css/index.css">
-
-
-        <title></title>
-    </head>
     <body class="bg-vert">
         <div class=" container d-flex flex-column">
             <h1 class="text-center mt-2 mb-5 titre">Bienvenue chez Paradise Coffee!!</h1>
