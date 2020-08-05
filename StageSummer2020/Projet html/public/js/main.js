44a7cd78375878525e2ab2051aed522fb830c899
AudreyBclz <61549680+AudreyBclz@users.noreply.github.com>
2020-08-05 18:54:52 +0200

    suite du html

--- StageSummer2020/Projet html/public/js/main.js
+++ StageSummer2020/Projet html/public/js/main.js
@@ -22,20 +22,29 @@ function myFunction() {
     if (window.pageYOffset <= sticky2) {
 
         footer.classList.add('sticky2');
-    } else {
-        footer.classList.remove('sticky2');
     }
 }
 
+function hauteur(block,block2)
+{
+    if (block.clientHeight < screen.availHeight) {
+        block2.css('height', '91vh');
 
-if (document.getElementById('second').clientHeight < screen.availHeight) {
-    $("#second").css('height', '91vh');
-    $("#second").css('z-index',0);
-} else {
-    $("#second").css('height', document.getElementById('second').height);
+    } else {
+        block2.css('height', block.height);
+    }
 }
+hauteur(document.getElementById('second'),$('#second'));
+hauteur(document.getElementById('third'),$('#third'));
+hauteur(document.getElementById('fourth'),$('#fourth'));
+hauteur(document.getElementById('fifth'),$('#fifth'));
+hauteur(document.getElementById('fifth'),$('#sixth'));
 
+$('a[href="https://elfsight.com/instagram-feed-instashow/?utm_source=websites&utm_medium=clients&utm_content=instagram-feed&utm_term=undefined&utm_campaign=free-widget"]').css('display','none !important');
 
+$('#first').on('click',function () {
+    $("#accueil").addClass('slide-in-left')
+});
 $('#first').on('mouseenter',function () {
 $("#accueil").addClass('slide-in-left')
 });
@@ -43,7 +52,10 @@ $('#first').on('mouseleave',function () {
     $("#accueil").removeClass('slide-in-left')
 });
 
-
+$('#second').on('click',function () {
+    $("#titreBio").addClass('slide-in-left');
+    $("#titreBio").removeClass('invisible')
+});
 $('#second').on('mouseenter',function () {
     $("#titreBio").addClass('slide-in-left');
     $("#titreBio").removeClass('invisible')
@@ -53,7 +65,10 @@ $('#second').on('mouseleave',function () {
     $("#titreBio").addClass('invisible');
 });
 
-
+$('#second').on('click',function () {
+    $("#col1Bio").addClass('slide-in-bottom');
+    $("#col1Bio").removeClass('invisible');
+});
 $('#second').on('mouseenter',function () {
     $("#col1Bio").addClass('slide-in-bottom');
     $("#col1Bio").removeClass('invisible');
@@ -63,7 +78,10 @@ $('#second').on('mouseleave',function () {
     $("#col1Bio").addClass('invisible');
 });
 
-
+$('#second').on('click',function () {
+    $("#col2Bio").addClass('slide-in-bottom');
+    $("#col2Bio").removeClass('invisible');
+});
 $('#second').on('mouseenter',function () {
     $("#col2Bio").addClass('slide-in-bottom');
     $("#col2Bio").removeClass('invisible');
@@ -73,3 +91,65 @@ $('#second').on('mouseleave',function () {
     $("#col2Bio").addClass('invisible');
 });
 
+
+$('#third').on('click',function () {
+    $("#gta").addClass('slide-in-top');
+    $("#gta").removeClass('invisible')
+});
+$('#third').on('mouseenter',function () {
+    $("#gta").addClass('slide-in-top');
+    $("#gta").removeClass('invisible')
+});
+$('#third').on('mouseleave',function () {
+    $("#gta").removeClass('slide-in-top');
+    $("#gta").addClass('invisible');
+});
+
+$('#third').on('click',function () {
+    $("#col1gta").addClass('slide-in-left');
+    $("#col1gta").removeClass('invisible');
+});
+$('#third').on('mouseenter',function () {
+    $("#col1gta").addClass('slide-in-left');
+    $("#col1gta").removeClass('invisible');
+});
+$('#third').on('mouseleave',function () {
+    $("#col1gta").removeClass('slide-in-left');
+    $("#col1gta").addClass('invisible');
+});
+
+$('#third').on('click',function () {
+    $("#col2gta").addClass('slide-in-right');
+    $("#col2gta").removeClass('invisible');
+});
+$('#third').on('mouseenter',function () {
+    $("#col2gta").addClass('slide-in-right');
+    $("#col2gta").removeClass('invisible');
+});
+$('#third').on('mouseleave',function () {
+    $("#col2gta").removeClass('slide-in-right');
+    $("#col2gta").addClass('invisible');
+});
+
+
+$('#fifth').on('click',function () {
+    $("#insta").addClass('slide-in-top');
+    $("#insta").removeClass('invisible')
+});
+$('#fifth').on('mouseenter',function () {
+    $("#insta").addClass('slide-in-top');
+    $("#insta").removeClass('invisible')
+});
+$('#fifth').on('mouseleave',function () {
+    $("#insta").removeClass('slide-in-top');
+    $("#insta").addClass('invisible');
+});
+
+$('#fifth').on('click',function () {
+    $("#instaligne").addClass('slide-in-left');
+    $("#instaligne").removeClass('invisible');
+});
+$('#fifth').on('mouseenter',function () {
+    $("#instaligne").addClass('slide-in-left');
+    $("#instaligne").removeClass('invisible');
+});
