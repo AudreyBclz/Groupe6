0d89f967968d85bc3119c71cf97d726c73e1d6b7
AudreyBclz <61549680+AudreyBclz@users.noreply.github.com>
2020-08-13 17:18:53 +0200

    Suite gestion des messages

--- StageSummer2020/projet_symfony/src/Controller/AdminContactController.php
+++ StageSummer2020/projet_symfony/src/Controller/AdminContactController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Contact;
+use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminContactController extends AbstractController
@@ -13,10 +15,59 @@ class AdminContactController extends AbstractController
      */
     public function index()
     {
-        $msg=$this->getDoctrine()->getRepository(Contact::class)->findAll();
-        return $this->render('admin_contact/index.html.twig', [
+        if (is_null($this->getUser()))
+        {
+            return $this->redirectToRoute('home');
+        }
+        else
+        {
+            $msgNonlu=$this->getDoctrine()->getRepository(Contact::class)->findBy(['isRead'=>0]);
+            $msg=$this->getDoctrine()->getRepository(Contact::class)->findBy(['isDeleted'=>0]);
+            return $this->render('admin_contact/index.html.twig', [
+                'controller_name' => 'AdminContactController',
+                'messages'=>$msg,
+                'msgNonLu'=>$msgNonlu
+            ]);
+        }
+
+    }
+
+    /**
+     * @Route("/admin/read/{id}", name="admin_read")
+     */
+    public function read (Contact $contact)
+    {
+        $msgNonlu=$this->getDoctrine()->getRepository(Contact::class)->findBy(['isRead'=>0]);
+        $msg=$this->getDoctrine()->getRepository(Contact::class)->findBy(['isDeleted'=>0]);
+            $contact->setIsRead(true);
+            $this->getDoctrine()->getManager()->flush();
+            return $this->redirectToRoute('admin_contact');
+
+            return $this->render('admin_contact/index.html.twig',[
+                'controller_name' => 'AdminContactController',
+                'messages'=>$msg,
+                'msgNonLu'=>$msgNonlu
+            ]);
+    }
+
+    /**
+     * @Route("/admin/notread/{id}", name="admin_notread")
+     */
+    public function notread (Contact $contact)
+    {
+        $msgNonlu=$this->getDoctrine()->getRepository(Contact::class)->findBy(['isRead'=>0]);
+        $msg=$this->getDoctrine()->getRepository(Contact::class)->findBy(['isDeleted'=>0]);
+        $contact->setIsRead(false);
+        $this->getDoctrine()->getManager()->flush();
+        return $this->redirectToRoute('admin_contact');
+
+        return $this->render('admin_contact/index.html.twig',[
             'controller_name' => 'AdminContactController',
-            'messages'=>$msg
+            'messages'=>$msg,
+            'msgNonLu'=>$msgNonlu
         ]);
     }
+
+
+
 }
